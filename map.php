88fea9bdf110324218dc7e26159dd79c241039b4
Slavomir Kozar <slavo.kozar@gmail.com>
2018-09-19 22:16:07 +0200

    pridane viacere patra

--- map.php
+++ map.php
@@ -6,36 +6,36 @@
 <body>
 
 <div id="block">
-<?php for($floor = 1; $floor <= 1; $floor++): ?>
-    <div class="block-floor" data-floor="<?php echo $floor; ?>">
-        <div class="block-side block-east">
-            <?php for($room = 19; $room >= 1; $room--): ?>
-                <div class="block-room" data-room="<?php echo $room; ?>">
-                    <div class="block-cupboard"></div>
-                    <div class="block-door"></div>
+    <?php for ($floor = 1; $floor <= 5; $floor++): ?>
+        <div class="block-floor" data-floor="<?php echo $floor; ?>">
+            <div class="block-side block-east">
+                <div class="block-rooms">
+                    <?php for ($room = 19; $room >= 1; $room--): ?>
+                        <div class="block-room" data-room="<?php echo $room; ?>"></div>
+                    <?php endfor; ?>
                 </div>
-            <?php endfor; ?>
-            <div class="block-stairs"></div>
-            <div class="block-common"></div>
+
+                <div class="block-stairs"></div>
+                <div class="block-common"></div>
+            </div>
+            <center><?php echo 'patro' . $floor; ?></center>
+            <div class="block-side block-west">
+                <div class="block-common"></div>
+                <div class="block-stairs"></div>
+                <div class="block-rooms">
+                    <?php for ($room = 24; $room <= 42; $room++): ?>
+                        <div class="block-room" data-room="<?php echo $room; ?>"></div>
+                    <?php endfor; ?>
+                </div>
+
+
+            </div>
         </div>
-        <div class="block-side block-west">
-            <?php for($room = 42; $room >= 24; $room--): ?>
-                <div class="block-room" data-room="<?php echo $room; ?>"><?php echo $room; ?></div>
-            <?php endfor; ?>
-            <div class="block-stairs"></div>
-            <div class="block-common"></div>
-        </div>
-    </div>
 
 
-
-
-
-
-<?php endfor; ?>
+    <?php endfor; ?>
 
 </div>
-
 
 
 </body>
